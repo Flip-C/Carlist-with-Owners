80ec2c05626f5c9fa3ea965d0476844108768560
Philipp Biermann <S2010438061@fhooe.at>
2020-12-04 17:58:37 +0100

    Created Class Personlist

---
+++ Carlist with Owners/Carlist with Owners/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carlist_with_Owners
+{
+    class Personlist
+    {
+        #region members
+        #endregion
+
+        #region constructor
+        #endregion
+
+        #region methods
+        #endregion
+
+        #region properties
+        #endregion
+
+    }
+}
