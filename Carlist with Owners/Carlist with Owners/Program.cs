ea4944baee28bc43b6ebe07fc6097a12eda840ac
Philipp Biermann <S2010438061@fhooe.at>
2020-12-05 11:04:58 +0100

    Get some Points in main Programm Changed Carclass

--- Carlist with Owners/Carlist with Owners/Program.cs
+++ Carlist with Owners/Carlist with Owners/Program.cs
@@ -11,15 +11,53 @@ namespace Carlist_with_Owners
         static void Main(string[] args)
         {
 
-            Console.ReadLine();
-        }
-        static void Output(Personlist[]Personarray)
-        {
-            for (int i = 0; i < Personarray.GetLength(0); i++)
+            bool inpudend = true;
+
+            do
             {
-                Console.WriteLine(Personarray[i].print());
+                Console.WriteLine("Hallo willkommen zur Autoliste");
+                Console.WriteLine("Folegnde Möglichkeiten stehen Ihnen zur Verfügung");
+                Console.WriteLine("h -> Help\n s -> zeigt alle Autos in der Liste\n c -> legt ein neues Auto an\n n -> zeigt den nächsten Service eines Autos\n v -> gibt den Aktuellen Wert eines Autos an");
+                char myfunction= Console.ReadLine()[0];
+
+
+                if (myfunction=='h')//Help
+                {
+                    
+                }
+                else if (myfunction=='s')//show all cars
+                {
+
+                }
+                else if (myfunction == 'c')//create new car
+                {
+
+                }
+                else if (myfunction =='n')//nextservice
+                {
+
+                }
+                else if (myfunction=='v')//price
+                {
+
+                }
+
+
+
+                Console.WriteLine("Autoliste beenden? J/N");
+                char stopp = Console.ReadLine()[0];
+                switch (stopp)
+                {
+                    case 'J': inpudend = true;break;
+                    case 'j': inpudend = true;break;
+                    case 'N': inpudend = false;break;
+                    case 'n':inpudend = false;break;
+                }
             }
+            while (inpudend);
+            Console.ReadLine();
+            
         }
-        
+                
     }
 }
--- Carlist with Owners/Carlist with Owners/carspecifications.cs
+++ Carlist with Owners/Carlist with Owners/carspecifications.cs
@@ -9,12 +9,15 @@ namespace Carlist_with_Owners
     class carspecifications
     {
         #region members
-        int listenpreis;
-        int momentanpreis;
-        int anzahlJahrenK; //Anzahl Jahre nach Kauf
-        int baujahr;
-        int kilometeranzahl;
-
+        double listenpreis;
+        double momentanpreisY;
+        double momentanpreisK;
+        double anzahlJahrenK; //Anzahl Jahre nach Kauf
+        double baujahr;
+        double kilometerstand;
+        double eingegebenerKilometerstand;
+        double servicelimit;
+        double servicewarnung;
         #endregion
 
         #region constructor
@@ -24,31 +27,70 @@ namespace Carlist_with_Owners
         public int Listenpreis { get; set; }
         public int Kaufpreis { get; set; }
         public int Baujahr { get; set; }
-        public int Kilometeranzahl { get; set; }
+        public int Kilometerstand { get; set; }
         #endregion
 
         #region methods
-        public void PriceCalculator()
+        public void PriceCalculatorY()
         {
             //Berechnet den Preis des Fahrzeugs
-            if (momentanpreis>2000)
+            if (momentanpreisY>2000)
             {
-                momentanpreis = listenpreis * anzahlJahrenK; //Faktor Wertverlust pro Jahr ca 0.05% + Kilometerlaufleistung 
+                momentanpreisY = listenpreis * anzahlJahrenK*0.05; //Faktor Wertverlust pro Jahr ca 0.05% + Kilometerlaufleistung 
 
 
             }
+            else if(momentanpreisY<2000)
+            {
+                Console.WriteLine("Der Wert des Fahrzeugs liegt unter 2000€. Fahren Sie weiter bis der Rost euch scheidet");
+            }
         }
+
+
+
+        public void PriceCalculatorK()
+        {
+            //Berechnet den Preis des Fahrzeugs anhand der Kilometer
+            if (momentanpreisK > 2000)
+            {
+                momentanpreisK = listenpreis * kilometerstand * 0.01;
+            }
+            else if (momentanpreisK < 2000)
+            {
+                Console.WriteLine("Der Wert des Fahrzeugs liegt unter 2000€. Fahren Sie weiter bis der Rost euch scheidet");
+            }
+        }
+
 
         public void ServiceCalculator()
         {
             //Berechnet den Zeitpunkt bis zum nächsten Service
             //Service alle 10.000km oder alle 2 Jahre
-            int servicewarnung;
-            servicewarnung = servicelimit - kilometeranzahl;
+            servicelimit = eingegebenerKilometerstand + 10000;
+            servicewarnung = servicelimit - kilometerstand;
             //for jeden Serviceeintrag i + 10000km
             //neues Servicelimit - Kilometeranzahl
 
 
+        }
+
+        public void NewCar()
+        {
+            Console.WriteLine("Bitte geben Sie die Fahrzeugdaten ein");
+            Console.WriteLine("Besitzer eingeben");
+            string owner = Console.ReadLine();
+
+            Console.WriteLine("Marke eingeben");
+            string modell = Console.ReadLine();
+
+            Console.WriteLine("Baujahr eingeben");
+            baujahr = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Listenpreis eingeben");
+            listenpreis = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Aktueller Kilometerstand angeben");
+            kilometerstand = double.Parse(Console.ReadLine());
         }
         
 
